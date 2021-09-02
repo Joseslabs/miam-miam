c0c99639152532f00a3207bfac6169bf673f2080
Joseslabs <76791600+Joseslabs@users.noreply.github.com>
2021-09-02 21:30:20 +0100

    new

--- src/index.js
+++ src/index.js
@@ -4,12 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Header() {
+  return <div className="header">
+    <h1>Miam-Miam</h1>
+    <form className="form">
+      <input type="text" className="search" placeholder="Entrez votre requête ici"/>
+      <button type="submit">Rechercher</button>
+    </form>
+    <nav>
+      <a className="home" href="#home">Acceuil</a>
+      <a className="service" href="#service">Nos Services</a>
+      <a className="commande" href="#commande">Commandes</a>
+    </nav>
+  </div>;
+}
+function Section(){
+  return <div className="section">
+    <p>Bienvenue sur Miam-Miam</p>
+  </div>;
+}
+function Body() {
+  return <div className="main">
+    <Header />
+    <Section />
+  </div>;
+}
+let main= <Body />;
+ReactDOM.render(main,document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
